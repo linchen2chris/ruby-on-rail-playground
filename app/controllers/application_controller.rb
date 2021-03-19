33d648cab1f2668bd7afdcbe9aa16a6df2cab51f
Chen Lin <chen.lin@rea-group.com>
2021-03-19 16:41:28 +0800

    chen create db

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   def greet
     render json: { message: 'hello chris' }
   end
+  def create
+    newobj = Item.new(:name => 'chen', :description => 'test')
+    newobj.save
+    render json: { message: 'success' }
+  end
 end
