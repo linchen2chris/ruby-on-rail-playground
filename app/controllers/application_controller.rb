daac7c8c7fd9bfa55d81a560a4d9f2cacebbf749
Chen Lin <chen.lin@rea-group.com>
2021-04-06 21:29:57 +0800

    chen change get to post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,12 @@
 class ApplicationController < ActionController::Base
+  skip_before_action :verify_authenticity_token
   def greet
     render json: { message: 'hello chris' }
   end
   def create
-    newobj = Item.new(:name => 'chen', :description => 'test')
+    newobj = Item.new(:name => params[:name], :description => params[:description])
+    puts newobj
     newobj.save
-    render json: { message: 'success' }
+    render json: { message: 'success11' }
   end
 end
